2cc99c3036cf7d79c0ed1abe6fc9c663bc436c22
PratikshaSh <pratiksha8520@gmail.com>
2021-03-16 19:54:39 +0530

    add authenticated delete button on complaints

--- routes/complaints.js
+++ routes/complaints.js
@@ -5,6 +5,6 @@ const passport = require('passport');
 const complaintController = require('../controllers/complaint_controller');
 router.post('/create',passport.checkAuthentication,complaintController.create);
 router.get('/section',passport.checkAuthentication, complaintController.complaint);
-
+router.get('/destroy/:id', passport.checkAuthentication, complaintController.destroy);
 
 module.exports = router;
