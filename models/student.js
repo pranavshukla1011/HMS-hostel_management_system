b20f87ad6e0ccafe7b6f2668adaa381a09b4bb96
PratikshaSh <pratiksha8520@gmail.com>
2021-03-17 10:41:39 +0530

    render all updated and new  pages

--- models/student.js
+++ models/student.js
@@ -11,6 +11,10 @@ const studentSchema = new mongoose.Schema({
             type: String,
             required: true
         },
+        hostel:{
+            type: String,
+            required: true
+        },
         
         aadharNo:{
             type: String,
@@ -21,7 +25,44 @@ const studentSchema = new mongoose.Schema({
             required: true,
             unique:true
         },
+        email2: {
+            type: String,
+            required: true,
+            unique:true
+        },
         name:{
+            type: String,
+            required : true
+        },
+        dob: {
+            type: String,
+            required : true
+        },
+        branch: {
+            type: String,
+            required : true
+        },
+        batch:{
+            type: String,
+            required : true
+        },
+        phone1: {
+            type: String,
+            required : true
+        },
+        fatherName: {
+            type: String,
+            required : true
+        },
+        motherName:{
+            type: String,
+            required : true
+        },
+        parentEmail: {
+            type: String,
+            required : true
+        },
+        parentNo: {
             type: String,
             required : true
         }
--- routes/students.js
+++ routes/students.js
@@ -8,6 +8,10 @@ router.get('/profile',passport.checkAuthentication ,studentsController.profile);
 router.get('/sign-up', studentsController.signUp);
 router.get('/sign-in', studentsController.signIn);
 router.get('/student-home',passport.checkAuthentication, studentsController.studentHome);
+router.get('/mess-leave',passport.checkAuthentication, studentsController.messLeave);
+router.get('/mark-holiday',passport.checkAuthentication, studentsController.markHoliday);
+
+
 router.post('/create',studentsController.create);
 
 //using passport as a middleware to authentication
